f8f6f55b6adc786dd805a12367cc8e25b394325f
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2016-12-17 20:42:54 -0300

    Chips

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { spring, Motion, presets } from 'react-motion'
 import { Board } from './crossword'
 import Controls from './components/Controls'
 import BoardComponent from './components/Board'
+import AppBar from 'react-toolbox/lib/app_bar'
 import 'react-toolbox/lib/commons.scss'
 import './index.css'
-
+import styles from './App.scss'
 export default class App extends Component {
   constructor () {
     super()
     this.changeWords = this.changeWords.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
     this.state = {
       words: ['hola']
     }
@@ -24,13 +26,29 @@ export default class App extends Component {
     }
   }
 
+  handleDelete (i) {
+    console.log(this.state.words.slice(0, i))
+    console.log(this.state.words.slice(i + 1))
+    this.setState({
+      words: [...this.state.words.slice(0, i), ...this.state.words.slice(i + 1)]
+    })
+    console.log(this.state.words)
+  }
+
   render() {
     const b = Board.from_words(this.state.words)
     const arr = b.toArray()
     return (
-      <div className="App">
-        <Controls onChange={this.changeWords} />
-        <BoardComponent arr={arr} />
+      <div className={styles.wrapper}>
+        <AppBar title='Crossword generator' />
+        <div className={styles.content}>
+          <div className={styles.controls}>
+            <Controls onChange={this.changeWords} words={this.state.words} onDelete={this.handleDelete} />
+          </div>
+          <div className={styles.board}>
+            <BoardComponent arr={arr} />
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,25 +1,43 @@
 import React from 'react'
 import _ from 'lodash'
+import Chip from 'react-toolbox/lib/chip'
+import { Button } from 'react-toolbox/lib/button'
+import Input from 'react-toolbox/lib/input'
 
 export default class Controls extends React.Component {
   constructor () {
     super()
-    this.onChange = this.onChange.bind(this)
-  }
-
-  onChange () {
-    const newWords = this.refs.input.value.split(' ')
-    if (!_.isEqual(newWords, this.state.words)) {
-      this.state.words = newWords
-      this.props.onChange(newWords)
+    this.addWord = this.addWord.bind(this)
+    this.changeInput = this.changeInput.bind(this)
+    this.state = {
+      input: ''
     }
   }
 
+  addWord (e) {
+    e.preventDefault()
+    const word = this.state.input
+    this.setState({input: ''})
+    const { words, onChange } = this.props
+    onChange(_.concat(words, word))
+  }
+
+  changeInput (v) {
+    console.log(v)
+    this.setState({input: v})
+  }
+
   render () {
-    const { onChange } = this.props
+    const { words, onDelete } = this.props
     return (
       <div>
-        <input type='text' ref='input' onChange={(e) => {onChange(e.target.value.split(' '))}}/>
+        <form onSubmit={this.addWord}>
+          <Input type='text' value={this.state.input} onChange={this.changeInput} />
+          <Button type='submit' onClick={this.addWord} label='Add' primary raised />
+        </form>
+        <div>
+          {words.map((w, i) => <Chip key={w} deletable onDeleteClick={() => onDelete(i)}>{w}</Chip>)}
+        </div>
       </div>
     )
   }
